4b8beb39c525eb51a9727e49b81877b3ba98a794
0hmori <mehowomori.fiofio.cute.38@gmail.com>
2023-10-13 20:26:45 +0900

    リマインダー修正20:32

--- remind.py
+++ remind.py
@@ -21,11 +21,20 @@ JST = timezone(timedelta(hours=+9), "JST")
 
 load_dotenv()
 
-app = Flask(__name__)
-
 
 line_bot_api = LineBotApi(os.environ["ACCESS_TOKEN"])
 handler = WebhookHandler(os.environ["CHANNEL_SECRET"])
+
+
+locale.setlocale(locale.LC_TIME, "ja_JP.UTF-8")
+Today = datetime.datetime.now()
+week_num = Today.weekday()
+w_list = ["月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日", "日曜日"]
+
+# print(Today,  week_num,  w_list[week_num]) 実行OK
+
+
+app = Flask(__name__)
 
 
 # @app.route("/push_sample")
@@ -37,29 +46,7 @@ def push_sample():
     return "OK"
 
 
-# @app.route("/callback", methods=["GET", "POST"])
-# def callback():
-#    signature = request.headers["X-Line-Signature"]
-#    body = request.get_data(as_text=True)
-#    app.logger.info("Request body: " + body)
-#
-#    try:
-#        handler.handle(body, signature)
-#    except InvalidSignatureError:  # as e:を消した！
-#        abort(400)
-#
-#    return "OK"
-#
-
-locale.setlocale(locale.LC_TIME, "ja_JP.UTF-8")
-Today = datetime.datetime.now()
-week_num = Today.weekday()
-w_list = ["月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日", "日曜日"]
-
-# print(Today,  week_num,  w_list[week_num]) 実行OK
-
-
-# rimind_punch_in関数のなかに、LINEに送るコードを入れた。メッセージ送られず↓
+# rimind_punch_in関数のなかに、LINEに送るコードを入れた。メッセージ送られず
 
 
 # @app.route("/rimind_punch_in")
@@ -100,50 +87,22 @@ def rimind_punch_in():
 # rimind_punch_in()  # 動作はOK！
 
 
-# push_sample()にrimind_punch_in関数を入れた。動作せず
-# def rimind_punch_in():
-#    if week_num == 0:
-#        message = "おはようございます！今日は月曜日です。出勤登録をお願いします"
-#        # LINE_Notify.Sent_Message(message)
-#    elif week_num == 1:
-#        message = "おはようございます！今日は火曜日です。出勤登録をお願いします"
-#    elif week_num == 2:
-#        message = "おはようございます！今日は水曜日です。出勤登録をお願いします"  # 動作確認用。動作OK！print(rimind_punch_in())ではこのメッセージのみ出せた
-#    elif week_num == 3:
-#        message = "おはようございます！今日は木曜日です。出勤登録をお願いします"
-#    elif week_num == 4:
-#        message = "おはようございます！今日は金曜日です。出勤登録をお願いします"
-#    else:
-#        pass
-#    # print(w_list[week_num], message) 実行OK
-#    return message  # なんで赤波線が出るのか…
-
-
-# @app.route("/push_sample")
-# def push_sample():
-#    """プッシュメッセージを送る"""
-#    user_id = os.environ["USER_ID"]
-#    line_bot_api.push_message(user_id, TextSendMessage(rimind_punch_in()))
-#
-#    return "OK"
-
-
 if __name__ == "__main__":
-    schedule.every().friday.at("20:21").do(rimind_punch_in)
+    schedule.every().friday.at("20:32").do(rimind_punch_in)
 
-#    flask_thread = threading.Thread(target=app.run, kwargs={'debug': False})
-#    flask_thread.start()
+    flask_thread = threading.Thread(target=app.run, kwargs={'debug': False})
+    flask_thread.start()
 
     while True:
         schedule.run_pending()
         time.sleep(1)
 
-#if __name__ == "__main__":
+# if __name__ == "__main__":
 #    port = int(os.getenv("PORT", 5000))
 #    app.run(host="0.0.0.0", port=port)
 #
 #
-#if __name__ == "__main__":
+# if __name__ == "__main__":
 #    schedule.every().monday.at("08:30").do(rimind_punch_in)
 #    schedule.every().tuesday.at("08:30").do(rimind_punch_in)
 #    schedule.every().wednesday.at("08:30").do(rimind_punch_in)
